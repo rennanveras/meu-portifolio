56ae4fcc2090a5778087bc41ac1b96e149ca7deb
rennan veras <rennanhenriqueneves@gmail.com>
2023-10-09 15:03:30 -0300

    feat: adicionando a funcionalidade de contagem da minha idade toda vez que chega a data do meu aniversario minha idade é atualizada na pagina

--- src/components/HeroLeft/index.tsx
+++ src/components/HeroLeft/index.tsx
@@ -1,7 +1,35 @@
 import { RedeSocialList } from '../RedeSocialList';
 import { HeroLeftStyles } from './styles'
+import { useState, useEffect } from 'react';
 
 const HeroLeft = () => {
+
+  const [age, setAge] = useState(0);
+
+  const dateOfBirth = new Date('2003-12-07');
+
+  useEffect(() => {
+    const calculateAge = () => {
+      const today = new Date();
+      let age = today.getFullYear() - dateOfBirth.getFullYear();
+
+      const birthDateThisYear = new Date(today.getFullYear(), dateOfBirth.getMonth(), dateOfBirth.getDate());
+
+      if (today < birthDateThisYear) {
+        age--;
+      }
+
+      setAge(age);
+    };
+
+    calculateAge();
+
+    const intervalId = setInterval(() => {
+      calculateAge();
+    }, 24 * 60 * 60 * 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <HeroLeftStyles className='hero-left'>
@@ -10,7 +38,7 @@ const HeroLeft = () => {
           <h2>
             Olá, Eu sou o <span className=''>Rennan Veras</span>
           </h2>
-          <p>Tenho 19 anos e estou em busca contínua de aprofundar meu conhecimento na área de programação, enquanto curso Análise e Desenvolvimento de Sistemas. O interesse pela programação surgiu a partir do meu envolvimento com jogos, e desde então, tenho me dedicado incansavelmente a estudar e aprimorar minhas habilidades em algo que verdadeiramente me apaixona. Estou comprometido com o aprendizado constante e ansioso para explorar ainda mais as emocionantes possibilidades que o mundo da programação tem a oferecer</p>
+          <p>Tenho {age} anos e estou em busca contínua de aprofundar meu conhecimento na área de programação, enquanto curso Análise e Desenvolvimento de Sistemas. O interesse pela programação surgiu a partir do meu envolvimento com jogos, e desde então, tenho me dedicado incansavelmente a estudar e aprimorar minhas habilidades em algo que verdadeiramente me apaixona. Estou comprometido com o aprendizado constante e ansioso para explorar ainda mais as emocionantes possibilidades que o mundo da programação tem a oferecer</p>
         </div>
         <div className='hero-left-redeSocial'>
           <h3>Onde me encrontrar</h3>
